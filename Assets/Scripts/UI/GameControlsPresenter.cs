e00c0ae991461ae7a717e25aa499cd41477cc682
aplotnikov <aplotnikov@gear-games.com>
2024-10-01 23:58:48 +0300

    UI для управления состоянием игры

---
+++ Assets/Scripts/UI/GameControlsPresenter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using ShootEmUp;
+using UnityEngine;
+using Utils;
+
+namespace UI
+{
+    public class GameControlsPresenter : MonoBehaviour
+    {
+        private const int COUNTDOWN_NUMBER = 3;
+        private const int COUNTDOWN_DELAY_SEC = 1;
+
+        private const string START_BUTTON_NAME = "Start";
+        private const string RESUME_BUTTON_NAME = "Resume";
+        private const string PAUSE_BUTTON_NAME = "Pause";
+
+        [SerializeField] private GameControlsView _controlsView;
+        [SerializeField] private GameManager _gameManager;
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            _controlsView.OnStartResumeButtonClicked += OnStartResumeButtonClicked;
+            _controlsView.OnPauseButtonClicked += OnPauseButtonClicked;
+        }
+
+        [UsedImplicitly]
+        private void Start() =>  _controlsView.ShowStartResumeButton(START_BUTTON_NAME);
+
+        [UsedImplicitly]
+        private void OnDestroy()
+        {
+            _controlsView.OnStartResumeButtonClicked -= OnStartResumeButtonClicked;
+            _controlsView.OnPauseButtonClicked -= OnPauseButtonClicked;
+        }
+
+        private void OnStartResumeButtonClicked()
+        {
+            _controlsView.HideStartResumeButton();
+
+            Action onFinished = _gameManager.IsNotStarted
+                ? _gameManager.StartGame
+                : _gameManager.ResumeGame;
+
+            StartCoroutine(CountdownCoroutine(onFinished));
+        }
+
+        private void OnPauseButtonClicked()
+        {
+            _gameManager.PauseGame();
+            _controlsView.HidePauseButton();
+            _controlsView.ShowStartResumeButton(RESUME_BUTTON_NAME);
+        }
+
+        private IEnumerator CountdownCoroutine(Action onFinished)
+        {
+            _controlsView.ShowCountdown();
+
+            for (int number = COUNTDOWN_NUMBER; number > 0; number--)
+            {
+                _controlsView.SetCountdownValue(number.ToString());
+                yield return new WaitForSeconds(COUNTDOWN_DELAY_SEC);
+            }
+
+            _controlsView.HideCountdown();
+            _controlsView.ShowPauseButton(PAUSE_BUTTON_NAME);
+            onFinished.SafeInvoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameControlsView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+
+namespace UI
+{
+    public class GameControlsView : MonoBehaviour
+    {
+        public event Action OnStartResumeButtonClicked;
+        public event Action OnPauseButtonClicked;
+
+        [SerializeField] private TMP_Text _countdownText;
+
+        [SerializeField] private Button _startResumeButton;
+        [SerializeField] private TMP_Text _startResumeButtonText;
+
+        [SerializeField] private Button _pauseButton;
+        [SerializeField] private TMP_Text _pauseButtonText;
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            HideCountdown();
+            HideStartResumeButton();
+            HidePauseButton();
+
+            _startResumeButton.onClick.AddListener(OnStartResumeClick);
+            _pauseButton.onClick.AddListener(OnPauseClick);
+        }
+
+        [UsedImplicitly]
+        private void OnDestroy()
+        {
+            _startResumeButton.onClick.RemoveListener(OnStartResumeClick);
+            _pauseButton.onClick.RemoveListener(OnPauseClick);
+        }
+
+        public void ShowCountdown() => _countdownText.gameObject.SetActive(true);
+
+        public void HideCountdown() => _countdownText.gameObject.SetActive(false);
+
+        public void SetCountdownValue(string text) => _countdownText.SetText(text);
+
+        public void ShowStartResumeButton(string buttonText)
+        {
+            _startResumeButtonText.text = buttonText;
+            _startResumeButton.gameObject.SetActive(true);
+        }
+
+        public void HideStartResumeButton() => _startResumeButton.gameObject.SetActive(false);
+
+        public void ShowPauseButton(string buttonText)
+        {
+            _pauseButtonText.text = buttonText;
+            _pauseButton.gameObject.SetActive(true);
+        }
+
+        public void HidePauseButton() => _pauseButton.gameObject.SetActive(false);
+
+        private void OnStartResumeClick() => OnStartResumeButtonClicked.SafeInvoke();
+
+        private void OnPauseClick() => OnPauseButtonClicked.SafeInvoke();
+    }
+}
