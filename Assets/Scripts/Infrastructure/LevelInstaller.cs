b39bd95e04fb0ec513e55b6bbc905cd09d7974fe
aplotnikov <aplotnikov@gear-games.com>
2024-11-10 14:42:56 +0300

    Удалены файлы предыдущей домашки

---
+++ Assets/Scripts/Infrastructure/LevelInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Infrastructure
+{
+    public class LevelInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
