3fc913c43735e17c10705fee51156310ea226a83
aplotnikov <aplotnikov@gear-games.com>
2024-11-13 21:08:28 +0300

    EntryPoint заменен на Zenject + DataHelper

---
+++ Assets/Scripts/Helpers/DataHelper.cs
@@ -0,0 +1,35 @@
+﻿using GameEngine;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Helpers
+{
+    public class DataHelper : MonoBehaviour
+    {
+        [ShowInInspector]
+        private UnitManager _unitManager;
+
+        [ShowInInspector, ReadOnly]
+        private ResourceService _resourceService;
+
+        [Inject]
+        public void Construct(UnitManager unitManager, ResourceService resourceService)
+        {
+            _unitManager = unitManager;
+            _resourceService = resourceService;
+        }
+
+        [Button("Загрузить файл")]
+        public void Load()
+        {
+            // TODO
+        }
+
+        [Button("Сохранить в файл")]
+        public void Save()
+        {
+            // TODO
+        }
+    }
+}
--- Assets/Scripts/Infrastructure/LevelInstaller.cs
+++ Assets/Scripts/Infrastructure/LevelInstaller.cs
@@ -1,12 +1,22 @@
-﻿using Zenject;
+﻿using GameEngine;
+using UnityEngine;
+using Zenject;
 
 namespace Infrastructure
 {
     public class LevelInstaller : MonoInstaller
     {
+        [SerializeField] private Transform _unitsContainer;
+
         public override void InstallBindings()
         {
+            UnitManager unitManager = new(_unitsContainer);
+            unitManager.SetupUnits(FindObjectsOfType<Unit>());
+            Container.BindInstance(unitManager).AsSingle();
 
+            ResourceService resourceService = new();
+            resourceService.SetResources(FindObjectsOfType<Resource>());
+            Container.BindInstance(resourceService).AsSingle();
         }
     }
 }
