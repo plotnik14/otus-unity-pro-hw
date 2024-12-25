1a86c11aaab2da5d5db550df3f5f4023da050a47
aplotnikov <aplotnikov@gear-games.com>
2024-12-25 21:19:15 +0300

    Убраны ненужные файлы

--- Assets/Scripts/Infrastructure/LevelInstaller.cs
+++ Assets/Scripts/Infrastructure/LevelInstaller.cs
@@ -1,51 +1,12 @@
-﻿using GameEngine;
-using SaveSystem;
-using SaveSystem.GameRepositories;
-using SaveSystem.GameRepositories.StateLoadStrategies;
-using SaveSystem.SaveLoaders;
-using UnityEngine;
-using Zenject;
+﻿using Zenject;
 
 namespace Infrastructure
 {
     public class LevelInstaller : MonoInstaller
     {
-        [SerializeField] private Transform _unitsContainer;
-        [SerializeField] private UnitPrefabsConfiguration _unitPrefabsConfiguration;
-
         public override void InstallBindings()
         {
-            Container.BindInstance(_unitPrefabsConfiguration);
-            BindUnitManager();
-            BindResourceService();
-            BindRepository();
-            BindSaveLoaders();
-            Container.Bind<SaveLoadManager>().AsSingle();
-        }
 
-        private void BindUnitManager()
-        {
-            UnitManager unitManager = new(_unitsContainer);
-            Container.BindInstance(unitManager).AsSingle();
-        }
-
-        private void BindResourceService()
-        {
-            ResourceService resourceService = new();
-            Container.BindInstance(resourceService).AsSingle();
-        }
-
-        private void BindRepository()
-        {
-            Container.BindInterfacesTo<FileGameStateLoader>().AsSingle();
-            // Container.BindInterfacesTo<PlayerPrefsGameStateLoader>().AsSingle();
-            Container.BindInterfacesTo<GameStateRepository>().AsSingle();
-        }
-
-        private void BindSaveLoaders()
-        {
-            Container.BindInterfacesTo<ResourcesSaveLoader>().AsSingle();
-            Container.BindInterfacesTo<UnitsSaveLoader>().AsSingle();
         }
     }
 }
