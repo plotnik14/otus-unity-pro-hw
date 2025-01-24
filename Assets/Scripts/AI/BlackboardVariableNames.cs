c0e7e30aa4c6e731e942fc94166e2fb2c5e32232
aplotnikov <aplotnikov@gear-games.com>
2025-01-24 22:26:43 +0300

    Удаление всего лишнего после AI домашки

--- Assets/Scripts/AI/BlackboardVariableNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AI
-{
-    public static class BlackboardVariableNames
-    {
-        public const string HAS_AVAILABLE_TREE = "HasAvailableTree";
-        public const string COLLECTED_WOOD = "CollectedWood";
-    }
-}
