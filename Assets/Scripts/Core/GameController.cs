a9582aafbef20b7f9c54f12a5df112093c7e1157
aplotnikov <aplotnikov@gear-games.com>
2025-03-18 19:53:33 +0300

    Разбиение GameController на монобех, контроллер и фичу

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -1,64 +1,23 @@
 ﻿using Configs;
-using Core.Systems;
-using JetBrains.Annotations;
-using Services;
-using UnityEngine;
 
 namespace Core
 {
-    public class GameController : MonoBehaviour
+    public class GameController
     {
-        [SerializeField] private UnitConfig _unitConfig;
-        [SerializeField] private ProjectileConfig _projectileConfig;
-        [SerializeField] private SpawnArmyConfig _spawnArmyConfig;
+        private readonly Entitas.Systems _systems;
 
-        private Entitas.Systems _systems;
-
-        [UsedImplicitly]
-        private void Start()
+        public GameController(
+            Contexts contexts,
+            UnitConfig unitConfig,
+            ProjectileConfig projectileConfig,
+            SpawnArmyConfig spawnArmyConfig)
         {
-            ITimeService timeService = new UnityTimeService();
-            IAssetLoader assetLoader = new AssetLoader();
-            IGameObjectFactory objectFactory = new GameObjectFactory();
-            Contexts contexts = Contexts.sharedInstance;
-
-            _systems = new Feature("Systems")
-                // Initialize
-                .Add(new SpawnArmySystem(contexts.game, _unitConfig, _spawnArmyConfig))
-
-                // Execute
-                .Add(new MovementSystem(contexts.game, timeService))
-                .Add(new RotationSystem(contexts.game))
-                .Add(new UnitCollisionSystem(contexts.game, _projectileConfig))
-                .Add(new ProjectileCollisionSystem(contexts.game))
-                .Add(new DealDamageSystem(contexts.game))
-                .Add(new ReleaseTargetSystem(contexts.game))
-                .Add(new FindTargetSystem(contexts.game, _unitConfig))
-                .Add(new AttackCooldownSystem(contexts.game, timeService))
-                .Add(new AttackSystem(contexts.game))
-                .Add(new LookAtTargetSystem(contexts.game))
-                .Add(new SpawnProjectileSystem(contexts.game, _projectileConfig))
-                .Add(new AddAttackCooldownSystem(contexts.game, _unitConfig))
-                .Add(new LifeTimeSystem(contexts.game, timeService))
-
-                // View
-                .Add(new CreateViewSystem(contexts.game, assetLoader, objectFactory))
-
-                // Events (Generated)
-                .Add(new DestroyedEventSystem(contexts))
-                .Add(new PositionEventSystem(contexts))
-                .Add(new RotationEventSystem(contexts))
-                .Add(new TeamEventSystem(contexts))
-
-                // Cleanup
-                .Add(new AttackRequestCleanup(contexts.game))
-                .Add(new DestroyedCleanupSystem(contexts.game));
-
-            _systems.Initialize();
+            _systems = new GameSystems(contexts, unitConfig, projectileConfig, spawnArmyConfig);
         }
 
-        [UsedImplicitly]
-        private void Update()
+        public void Initialize() => _systems.Initialize();
+
+        public void Execute()
         {
             _systems.Execute();
             _systems.Cleanup();
---
+++ Assets/Scripts/Core/GameSystems.cs
@@ -0,0 +1,52 @@
+﻿using Configs;
+using Core.Systems;
+using Services;
+
+namespace Core
+{
+    public class GameSystems : Feature
+    {
+        public GameSystems(
+            Contexts contexts,
+            UnitConfig unitConfig,
+            ProjectileConfig projectileConfig,
+            SpawnArmyConfig spawnArmyConfig)
+        {
+            // ToDo DI
+            ITimeService timeService = new UnityTimeService();
+            IAssetLoader assetLoader = new AssetLoader();
+            IGameObjectFactory objectFactory = new GameObjectFactory();
+
+            // Initialize
+            Add(new SpawnArmySystem(contexts.game, unitConfig, spawnArmyConfig));
+
+            // Execute
+            Add(new MovementSystem(contexts.game, timeService));
+            Add(new RotationSystem(contexts.game));
+            Add(new UnitCollisionSystem(contexts.game, projectileConfig));
+            Add(new ProjectileCollisionSystem(contexts.game));
+            Add(new DealDamageSystem(contexts.game));
+            Add(new ReleaseTargetSystem(contexts.game));
+            Add(new FindTargetSystem(contexts.game, unitConfig));
+            Add(new AttackCooldownSystem(contexts.game, timeService));
+            Add(new AttackSystem(contexts.game));
+            Add(new LookAtTargetSystem(contexts.game));
+            Add(new SpawnProjectileSystem(contexts.game, projectileConfig));
+            Add(new AddAttackCooldownSystem(contexts.game, unitConfig));
+            Add(new LifeTimeSystem(contexts.game, timeService));
+
+            // View
+            Add(new CreateViewSystem(contexts.game, assetLoader, objectFactory));
+
+            // Events (Generated)
+            Add(new DestroyedEventSystem(contexts));
+            Add(new PositionEventSystem(contexts));
+            Add(new RotationEventSystem(contexts));
+            Add(new TeamEventSystem(contexts));
+
+            // Cleanup
+            Add(new AttackRequestCleanup(contexts.game));
+            Add(new DestroyedCleanupSystem(contexts.game));
+        }
+    }
+}
