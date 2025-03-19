51dd7d69b2d194b7def99265f653afd06715b84e
aplotnikov <aplotnikov@gear-games.com>
2025-03-19 21:37:17 +0300

    Логика отслеживания коллизий вынесена из общей вью в отдельную

---
+++ Assets/Scripts/View/TriggerHandlerView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace View
+{
+    public class TriggerHandlerView : MonoBehaviour
+    {
+        [SerializeField] private UnityView _unityView;
+
+        [UsedImplicitly]
+        private void OnTriggerEnter(Collider other)
+        {
+            UnityView otherUnityView = other.gameObject.GetComponent<UnityView>();
+            GameEntity collidedWith = otherUnityView.LinkedEntity;
+            AddCollision(collidedWith);
+        }
+        
+        private void AddCollision(GameEntity collidedWith)
+        {
+            List<GameEntity> collisionsList = GetOrCreateCollisionsList();
+            collisionsList.Add(collidedWith);
+        }
+
+        private List<GameEntity> GetOrCreateCollisionsList()
+        {
+            GameEntity linkedEntity = _unityView.LinkedEntity;
+
+            if (linkedEntity.hasCollisions)
+                return linkedEntity.collisions.list;
+
+            List<GameEntity> collisionsList = new();
+            linkedEntity.AddCollisions(collisionsList);
+            return collisionsList;
+        }
+    }
+}
--- Assets/Scripts/View/UnityView.cs
+++ Assets/Scripts/View/UnityView.cs
@@ -1,31 +1,21 @@
-﻿using System.Collections.Generic;
-using Entitas;
+﻿using Entitas;
 using Entitas.Unity;
-using JetBrains.Annotations;
 using UnityEngine;
 
 namespace View
 {
     public class UnityView : MonoBehaviour, IEntityView, IPositionListener, IRotationListener, IDestroyedListener
     {
-        private GameEntity _linkedEntity;
+        public GameEntity LinkedEntity { get; private set; }
 
         public void Link(IEntity entity)
         {
             gameObject.Link(entity);
-            _linkedEntity = (GameEntity)entity;
-            _linkedEntity.AddPositionListener(this);
-            _linkedEntity.AddRotationListener(this);
-            _linkedEntity.AddDestroyedListener(this);
-            OnLink(_linkedEntity);
-        }
-
-        [UsedImplicitly]
-        private void OnTriggerEnter(Collider other)
-        {
-            UnityView unityView = other.gameObject.GetComponent<UnityView>();
-            GameEntity collidedWith = unityView._linkedEntity;
-            AddCollision(collidedWith);
+            LinkedEntity = (GameEntity)entity;
+            LinkedEntity.AddPositionListener(this);
+            LinkedEntity.AddRotationListener(this);
+            LinkedEntity.AddDestroyedListener(this);
+            OnLink(LinkedEntity);
         }
 
         public void OnPosition(GameEntity entity, Vector3 value) => gameObject.transform.position = value;
@@ -36,25 +26,9 @@ namespace View
         {
             gameObject.Unlink();
             Destroy(gameObject);
-            _linkedEntity = null;
+            LinkedEntity = null;
         }
 
         protected virtual void OnLink(GameEntity linkedEntity) { }
-
-        private void AddCollision(GameEntity collidedWith)
-        {
-            List<GameEntity> collisionsList = GetOrCreateCollisionsList();
-            collisionsList.Add(collidedWith);
-        }
-
-        private List<GameEntity> GetOrCreateCollisionsList()
-        {
-            if (_linkedEntity.hasCollisions)
-                return _linkedEntity.collisions.list;
-
-            List<GameEntity> collisionsList = new();
-            _linkedEntity.AddCollisions(collisionsList);
-            return collisionsList;
-        }
     }
 }
